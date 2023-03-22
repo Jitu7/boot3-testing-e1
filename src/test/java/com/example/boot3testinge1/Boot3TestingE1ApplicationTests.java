2d75ee83f382632e4390bf925361726cacc84b2e
Jyotirmaya Das <jeetu@Jyotirmayas-MacBook-Air.local>
2023-03-22 13:34:15 +0530

    Initial commit

---
+++ src/test/java/com/example/boot3testinge1/Boot3TestingE1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boot3testinge1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boot3TestingE1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
